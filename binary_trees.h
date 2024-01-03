17c5898596fc2824e8d067c82dbdf1e04be616e7
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-03 19:24:26 +0300

    inprogress

--- binary_trees.h
+++ binary_trees.h
@@ -7,40 +7,6 @@
 #include <string.h>
 #include <limits.h>
 
-
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-/* Binary Search Tree */
-typedef struct binary_tree_s bst_t;
-
-/* AVL Tree */
-typedef struct binary_tree_s avl_t;
-
-/* Max Binary Heap */
-typedef struct binary_tree_s heap_t;
-
-/* Printing helper function */
-void binary_tree_print(const binary_tree_t *);
-
-/**
- * struct levelorder_queue_s - Level order traversal queue.
- * @node: A node of a binary tree.
- * @next: The next node to traverse to in the binary tree.
- */
-typedef struct levelorder_queue_s
-{
-    binary_tree_t *node;
-    struct levelorder_queue_s *next;
-} levelorder_queue_t;
-
-
 int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
