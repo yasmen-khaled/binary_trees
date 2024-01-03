0793518ccd10480c45a187bb59d35a0f591792df
yasmen <yasmen.khaled2000@gmail.com>
2024-01-03 20:50:32 +0200

    done

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - function that checks if a node is leaf
+ * @node: pointer
+ * Return: 1
   */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_root - function that checks if the node is root or not
+ * @node: pointer
+ * Return:1
   */
 
 int binary_tree_is_root(const binary_tree_t *node)
