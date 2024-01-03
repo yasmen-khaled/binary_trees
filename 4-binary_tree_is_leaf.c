f0b9773483cd259d35715f1b6d841e89e330b9e3
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-03 21:45:58 +0300

    almost done

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+  */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+
+	return (1);
+}
