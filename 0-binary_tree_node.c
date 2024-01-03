4f26fce0227f28c982a0586d192da83ca2bc2a71
yasmen <yasmen.khaled2000@gmail.com>
2024-01-03 20:29:03 +0200

    betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node - function that creates a binary tree node
- * parent:  node
+ * binary_tree_t - function that creates a binary tree node
+ * parent: node
  * value: new node
- * Return: _new
+ * Return:_new
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
@@ -14,13 +14,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	 _new = malloc(sizeof(binary_tree_t));
 
-	 if ( _new == NULL)
-		 return (NULL);
+	if (_new == NULL)
+		return (NULL);
 
 	  _new->n = value;
 	  _new->parent = parent;
 	  _new->left = NULL;
 	  _new->right = NULL;
 
-	 return (_new);
+	return (_new);
 }
