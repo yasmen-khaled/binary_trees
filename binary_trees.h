cd9f02938f3df85df90c32a5ec5f0bc3ba186599
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-03 19:19:10 +0300

    done

---
+++ binary_trees.h
@@ -0,0 +1,12 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int binary_tree_is_root(const binary_tree_t *node);
+
+#endif
