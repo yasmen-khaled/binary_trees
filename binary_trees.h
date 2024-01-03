841b3c54aea4876bc790606d2cc14518faa94098
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-03 20:43:30 +0300

    inprogress

--- binary_trees.h
+++ binary_trees.h
@@ -7,6 +7,18 @@
 #include <string.h>
 #include <limits.h>
 
+
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_root(const binary_tree_t *node);
 
 #endif
