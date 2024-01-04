55db790b0d10920416413cd00717877b0b39b4b7
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-04 23:31:30 +0300

    MoRe EneRgY  :'(

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_sibling - finds sibling node
+ *@node:pointer
+ *Return: node
+ */
+
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
+}
