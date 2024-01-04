c3ac9e5e03b4dc01759744b2927a7a191c8f0756
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-04 21:34:23 +0300

    more passion!!

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - = counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer
+ * Return: build
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t build = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		build += 1;
+	build += binary_tree_nodes(tree->left);
+	build += binary_tree_nodes(tree->right);
+	return (build);
+}
